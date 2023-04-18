aac63adee4196f9cc8bf6a9cef5031d5ac4a6a24
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-04-18 02:27:50 +0200

    init

---
+++ src/main/java/com/people/testowanieoprogramowaniazaawansowane130/TestowanieOprogramowaniaZaawansowane130Application.java
@@ -0,0 +1,13 @@
+package com.people.testowanieoprogramowaniazaawansowane130;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowanieOprogramowaniaZaawansowane130Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestowanieOprogramowaniaZaawansowane130Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/people/testowanieoprogramowaniazaawansowane130/TestowanieOprogramowaniaZaawansowane130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.testowanieoprogramowaniazaawansowane130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestowanieOprogramowaniaZaawansowane130ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
