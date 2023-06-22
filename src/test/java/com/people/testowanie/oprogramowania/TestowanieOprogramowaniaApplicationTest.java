2dc53e9dc7f4f331dffbbacc5738c909bb2d92f1
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-06-22 18:30:44 +0200

    prepared for classes

--- src/test/java/com/people/testowanie/oprogramowania/TestowanieOprogramowaniaApplicationTest.java
+++ src/test/java/com/people/testowanie/oprogramowania/TestowanieOprogramowaniaApplicationTest.java
@@ -1,11 +1,12 @@
 package com.people.testowanie.oprogramowania;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 public class TestowanieOprogramowaniaApplicationTest {
 
     @Test
-    void contextLoads() throws Exception {
+    void contextLoads() {
     }
 }
